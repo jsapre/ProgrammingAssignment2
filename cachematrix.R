c665d16908ab2485bd722862f807413532f9a8fe
jsapre <jsapre@users.noreply.github.com>
2015-04-21 00:25:10 -0400

    ProgrammingAssignment2 rprog-03 coursera - jsapre@hotmail.com

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This source file is part of submission of second programming assignment
+## for coursera course rprog-013 as submitted by jsapre@hotmail.com
+## The functions in this source file offer a wrapper around matrix
+## to calculate its inverse such that it is cached.
 
-## Write a short comment describing this function
+
+## For given invertible matrix x this function
+## gives back a wrapper list object that can be used
+## with cacheSolve to get cached inverse of the matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes the special object created by makeCacheMatrix
+## checks if inverse exists as cached matrix else calculates it and returns the same
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data)
+    x$setinverse(inverse)
+    inverse
 }
